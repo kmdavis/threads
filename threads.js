59d0ff9eb57d80cb0ee3bebbc6aba308e8e1322e
Kevan Davis <kdavis@gilt.com>
2011-05-20 14:00:25 -0400

    improved ability to reuse workers

--- threads.js
+++ threads.js
@@ -1,6 +1,6 @@
 (function threadWrapper () {
   if (window.Worker && (window.BlobBuilder || window.WebKitBlobBuilder) && (window.createBlobURL || window.URL || window.webkitURL)) {
-    var bb, threadPool = [], loaderSrc;
+    var bb, threadPool = [], loaderSrc, seq = 1;
 
     if (window.BlobBuilder) {
       bb = new BlobBuilder();
@@ -12,24 +12,26 @@
 
     bb.append("(" +
       (function threadLoader () {
-        console = {
-          log: function log () {
-            postMessage({
-              type: "log",
-              message: [].slice.apply(arguments)
-            });
-          }
-        };
+        if (!this.console) {
+          console = {
+            log: function log () {
+              postMessage({
+                type: "log",
+                message: [].slice.apply(arguments)
+              });
+            }
+          };
+        }
 
         var listeners = {};
 
         onmessage = function onMessage (ev) {
           var i, listening, context = {
-            listen: function (fn) {
+            listen: function listen (fn) {
               listening.push(fn);
             },
 
-            tell: function () {
+            tell: function tell () {
               postMessage({
                 type: "tell",
                 id: ev.data.id,
@@ -67,99 +69,111 @@
       throw new Error("No means to create a Blob URL");
     }
 
-    // Semi Fake Threading:  Execute in Worker
     window.Thread = function Thread (fn, args) {
       var
+        id = (new Date()).getTime().toString() + (seq += 1),
         worker = null,
-        result = null,
         failure = null,
-        events = {
-          done: [],
-          fail: [],
-          listen: []
-        }, i;
+        failCallbacks = [], i;
 
       if (0 === threadPool.length) {
         worker = new Worker(loaderSrc);
-      } else {
-        worker = threadPool.shift();
-      }
-      
-      worker.onmessage = function onmessage (ev) {
-        var args;
 
-        if ("done" === ev.data.type) {
-          result = ev.data.result;
-          for (i = 0; i < events.done.length; i += 1) {
-            events.done[i](result);
-          }
-          threadPool.push(worker);
-        } else if ("log" === ev.data.type) {
-          if (window.console && console.log) {
-            args = ev.data.message;
-            try {
+        worker.contexts = {};
 
-              // Firefox and Opera support using apply on native functions
-              console.log.apply(this, args);
+        worker.onmessage = function onmessage (ev) {
+          var args;
 
-            } catch (e) {
+          if ("done" === ev.data.type) {
 
-              // IE and Webkit do not allow using apply on native functions, so, we have to do this the hard and ugly way
-              switch (args.length) {
-              case 1:
-                console.log(args[0]);
-                break;
-              case 2:
-                console.log(args[0], args[1]);
-                break;
-              case 3:
-                console.log(args[0], args[1], args[2]);
-                break;
-              case 4:
-                console.log(args[0], args[1], args[2], args[3]);
-                break;
-              case 5:
-                console.log(args[0], args[1], args[2], args[3], args[4]);
-                break;
-              default:
-                console.log(args);
+            worker.contexts[ev.data.id].result = ev.data.result;
+            for (i = 0; i < worker.contexts[ev.data.id].events.done.length; i += 1) {
+              worker.contexts[ev.data.id].events.done[i](ev.data.result);
+            }
+            threadPool.push(worker);
+
+          } else if ("log" === ev.data.type) {
+
+            if (window.console && console.log) {
+              args = ev.data.message;
+              try {
+
+                // Firefox and Opera support using apply on native functions
+                console.log.apply(this, args);
+
+              } catch (e) {
+
+                // IE and Webkit do not allow using apply on native functions, so, we have to do this the hard and ugly way
+                switch (args.length) {
+                case 1:
+                  console.log(args[0]);
+                  break;
+                case 2:
+                  console.log(args[0], args[1]);
+                  break;
+                case 3:
+                  console.log(args[0], args[1], args[2]);
+                  break;
+                case 4:
+                  console.log(args[0], args[1], args[2], args[3]);
+                  break;
+                case 5:
+                  console.log(args[0], args[1], args[2], args[3], args[4]);
+                  break;
+                default:
+                  console.log(args);
+                }
               }
             }
-          }
-        } else if ("tell" === ev.data.type) {
-          for (i = 0; i < events.listen.length; i += 1) {
-            events.listen[i].apply(null, ev.data.args);
-          }
-        }
-      };
 
-      worker.onerror = function onerror (e) {
-        failure = e;
-        for (i = 0; i < events.fail.length; i += 1) {
-          events.fail[i](e);
+          } else if ("tell" === ev.data.type) {
+            
+            for (i = 0; i < worker.contexts[ev.data.id].events.listen.length; i += 1) {
+              worker.contexts[ev.data.id].events.listen[i].apply(null, ev.data.args);
+            }
+          }
+        };
+
+        worker.onerror = function onerror (e) {
+
+          failure = e;
+          for (i = 0; i < failCallbacks.length; i += 1) {
+            failCallbacks[i](e);
+          }
+          worker.terminate();
+        };
+      } else {
+        
+        worker = threadPool.shift();
+      }
+
+      worker.contexts[id] = {
+        result: null,
+        events: {
+          done: [],
+          listen: []
         }
-        worker.terminate();
       };
 
       this.done = function done (fn) {
-        if (result) {
-          fn(result);
+        if (worker.contexts[id].result) {
+          fn(worker.contexts[id].result);
         } else {
-          events.done.push(fn);
+          worker.contexts[id].events.done.push(fn);
         }
 
         return this;
       };
 
       this.listen = function listen (fn) {
-        events.listen.push(fn);
+        worker.contexts[id].events.listen.push(fn);
         return this;
       };
 
       this.tell = function tell () {
         worker.postMessage({
           type: "tell",
-          id: "only", // todo
+          id: id,
           args: [].slice.apply(arguments)
         });
 
@@ -170,7 +184,7 @@
         if (failure) {
           fn(failure);
         } else {
-          events.fail.push(fn);
+          failCallbacks.push(fn);
         }
 
         return this;
@@ -181,7 +195,7 @@
 
       worker.postMessage({ // this assumes complex data is allowed.  need a fallback / detection system
         type: "source",
-        id: "only", // todo
+        id: id,
         src: fn.toString(),
         args: args
       });
@@ -190,7 +204,11 @@
     Thread.usesWorkers = true;
   } else {
 
-    // Really Fake Threading:  Execute in UI Thread
+    if (window.console && console.warn) {
+      console.warn("Threading is not available -- falling back to executing in the UI Thread");
+    }
+
+    // Fallback:
     window.Thread = function Thread (fn, args) {
       var
         result = null,
@@ -202,14 +220,14 @@
         },
         i,
         context = {
-          tell: function () {
+          tell: function tell () {
             var args = [].slice.apply(arguments);
             for (i = 0; i < events.listen.length; i += 1) {
               events.listen[i].apply(null, args);
             }
           },
 
-          listen: function (fn) {
+          listen: function listen (fn) {
             listening.push(fn);
           }
         },
@@ -263,7 +281,7 @@
         return this;
       };
 
-      this.kill = function kill () {};
+      this.kill = function kill () { /* No Can Do*/ };
     };
 
     Thread.usesWorkers = false;
